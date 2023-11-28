bcbff0999fbc11d2cdf0de7c13355a8f0de66434
ssambadi <you@example.com>
2023-11-28 16:01:13 +0000

    more

--- js/verify_sessions.js
+++ js/verify_sessions.js
@@ -1,9 +1,9 @@
 const isSessionFound = () => {
-    let sessionId = sessionStorage.getItem('sessionID');
-    // let sessionId = getCookie('sessionId');
+    // let sessionId = sessionStorage.getItem('sessionID');
+    let sessionId = getCookie('sessionID');
     if (sessionId) {
         console.log("Session found: " + sessionId);
-        fetch('your-url-here', {
+        fetch('http://127.0.0.1:8080/verifySession', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -19,15 +19,15 @@ const isSessionFound = () => {
         console.log("No session found");
     }
 }
-// function getCookie(name) {
-//     let cookieArr = document.cookie.split(";");
-//     for(let i = 0; i < cookieArr.length; i++) {
-//         let cookiePair = cookieArr[i].split("=");
-//         if(name == cookiePair[0].trim()) {
-//             return decodeURIComponent(cookiePair[1]);
-//         }
-//     }
-//     return null;
-// }
+function getCookie(name) {
+    let cookieArr = document.cookie.split(";");
+    for(let i = 0; i < cookieArr.length; i++) {
+        let cookiePair = cookieArr[i].split("=");
+        if(name == cookiePair[0].trim()) {
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    return null;
+}
 
 export {isSessionFound}
